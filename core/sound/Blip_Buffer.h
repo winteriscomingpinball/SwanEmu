57f56cd2e0b72ecbe0f44130c188aa53c3172b6d
winteriscomingpinball <silentcatalyst@gmail.com>
2021-03-30 13:58:45 -0400

    Update Blip_Buffer.h

--- core/sound/Blip_Buffer.h
+++ core/sound/Blip_Buffer.h
@@ -98,7 +98,7 @@ blip_resampled_time_t Blip_Buffer_clock_rate_factor(Blip_Buffer* bbuf,
       long clock_rate);
 
 
-#define BLIP_BUFFER_ACCURACY 4
+#define BLIP_BUFFER_ACCURACY 6
 #define BLIP_PHASE_BITS 4
 
 // Number of bits in resample ratio fraction. Higher values give a more accurate ratio
